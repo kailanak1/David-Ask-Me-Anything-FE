e1b300fd0b0aef542c0434ec252877a066aee104
kailana <kailanakahawaii@gmail.com>
2020-06-04 15:54:15 -0700

    displays pic

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container'; 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
 
 
 
@@ -9,7 +14,21 @@ export default class About extends React.Component{
     render(){
         return(
             <div>
-            Hi. This is my About page. 
+                <Container fluid>
+                    <Row>
+                        <Col xs={6} md={4}>
+                            <Image 
+                            src={`${process.env.PUBLIC_URL}/assets/david-pic.jpg`} rounded
+                            alt="David" />
+                        </Col>
+                        
+                     
+                        <Col md={6} md={4}>
+                             Hi. This is my About page. 
+                        </Col>
+                       
+                    </Row>
+                </Container>
             </div>
         )
     };
