7f471f103b0e530294fd25f33f2dc7690eff477f
kailana <kailanakahawaii@gmail.com>
2020-06-05 10:13:03 -0700

    updated links

--- src/components/About.js
+++ src/components/About.js
@@ -24,10 +24,8 @@ export default class About extends React.Component{
                             src={`${process.env.PUBLIC_URL}/assets/david-pic.jpg`} rounded
                             alt="David" />
                         </Col>
-                        
-                     
                         <Col style={{"height":"667px"}} >
-                            <div style={{"height":"335px"}}> <a href="https://www.linkedin.com/in/david-d-589521b/" target="_blank">LinkedIn</a> Facebook Whatever </div>
+                            <div style={{"height":"335px"}}> <a href="https://www.linkedin.com/in/david-d-589521b/" target="_blank">LinkedIn</a> <a href="https://github.com/eckdd" target="_blank">Github</a>   </div>
                             <div style={{"height":"335px"}}> Hi. I'm David.  </div>
                         </Col>
                        
