fdbd54c4677fe921c8022fa5213c5cde4f09c4e6
kailana <kailanakahawaii@gmail.com>
2020-06-13 21:48:26 -0700

    added wine list component

---
+++ src/components/WineList.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+
+const WineList = () => {
+
+    const list = () => {
+        return(
+            <div>
+                <li>Montes Alpha</li>
+                <li>GSMs</li>
+            </div>
+        )
+    }
+
+
+    return(
+        {list}
+    )
+}
