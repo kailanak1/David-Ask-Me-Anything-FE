9cdb3822bf8cac671ec5d83a039248e170509287
kailana <kailanakahawaii@gmail.com>
2020-06-15 14:25:16 -0700

    added verification to email

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const EmailForm = ({coin}) => {
-  
 
 
-  handleSubmit = (event) => {
-	const templateId = 'template_id';
+	const [email, setEmail] = useState('')
+	const [error, setError] = useState(false)
 
-	this.sendFeedback(templateId, {message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email})
+	const handleEmailChange = e => {
+		setEmail(e.target.value)
+	}
+
+
+
+  const handleSubmit = (event) => {
+	  event.preventDefault()
+	 if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+		setError(true)
+	  } else {
+		const templateId = "david_personal_site";
+		const template_params = {
+			"email": email,
+			"coin": coin
+	 	}
+		 service_id: null
+		 API_KEY: null
+		sendCoin(service_id, templateId, template_params, API_KEY)
+	  }
+
+	
   }
 
-  sendFeedback = (templateId, variables) => {
+
+
+  
+
+  const sendCoin = (service_id, templateId, template_params, API_KEY) => {
+	  console.log(service_id, templateId, template_params, API_KEY)
 	window.emailjs.send(
-  	'gmail', templateId,
-  	variables
+  	service_id, templateId,
+  	template_params, API_KEY
   	).then(res => {
-    	console.log('Email successfully sent!')
+    	window.alert("Email sent!")
   	})
-  	// Handle errors here however you like, or use a React error boundary
   	.catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
   }
 
+
+
+
   return (
 	  <form onSubmit ={handleSubmit}>
-		  <input ></input>
+		  Email this code to me:
+		  <input placeholder="enter your email" value={email} onChange={handleEmailChange}></input>
+		  <input type="submit"></input>
+		  {!!error ? <p style={{color:"red"}}>Please enter a valid email address</p> : null}
 	  </form>
   )
 }
+
+export default EmailForm
